b642d069ed2708e0654504d8972c1bdebd960d69
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-08 12:56:19 +0300

    dsad

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,6 +1,9 @@
-export const baseUrl = process.env.SERVER_URI
+
+export const baseUrl = "https://chat-app-1-3wd3.onrender.com/api"
 
 export const PostRequest =  async (url, body) => {
+
+     console.log(baseUrl)
 
    const response =  await fetch(url, {
          method: "POST",
