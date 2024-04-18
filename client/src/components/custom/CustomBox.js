522d81caf1e0ddcf3b886440344af6f52d8d09d5
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-18 19:30:47 +0300

    sad

--- client/src/components/custom/CustomBox.js
+++ client/src/components/custom/CustomBox.js
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 
 const CustomBox = ({children}) => {
   return (
-    <Box p={5} component="section" display="flex" flexDirection="column" alignItems="center" width={520} borderRadius={3} height={400} backgroundColor="white" justifyContent="center" boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px">{children}</Box>
+    <Box p={5} ml={5} mr={5} component="section" display="flex" flexDirection="column" alignItems="center" width={520} borderRadius={3} height={400} backgroundColor="white" justifyContent="center" boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px">{children}</Box>
   )
 }
 
