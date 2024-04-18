284673f4fa152f3e790a2ee8dc6c0e69bde7c3a6
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-18 20:42:43 +0300

    sad

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -41,7 +41,7 @@ const Chat = () => {
             Loading chats...
           </Typography>
         )}
-        <Stack width="100%" height="100%" p={isMobile ? 1 : null} ml={ potentialChats?.length >= 1 || isMobile ? 16 : 0 }>{chats}</Stack>
+        <Stack width="100%" height="100%" p={isMobile ? 1 : null} ml={ potentialChats?.length >= 1 || !isMobile ? 16 : 0 }>{chats}</Stack>
 
         <Stack backgroundColor="#245DE6" height="90vh" position={ isMobile ? null : "fixed"} left="0">
           <PotentialChats />
